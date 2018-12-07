533dfad0d013b0bbddee5ab44304eb9b75bd85f9
Norrland97 <vikfre@student.chalmers.se>
2018-12-07 09:26:13 +0100

    Started with Vehicle app

--- src/main/VehicleApplication.java
+++ src/main/VehicleApplication.java
@@ -1,7 +1,37 @@
 package main;
 
+import main.controllers.CarController;
+import main.model.vehicles.CarFactory;
+import main.model.vehicles.cars.Car;
+import main.views.CarView;
+
 public class VehicleApplication {
 
+    CarView frame;
+
+
+    CarController cc = new CarController();
+
+    public void main(String[] args) {
+        // Instance of this class
+
+
+        cc.cars.add(CarFactory.createVolvo240());
+
+        Car saab = CarFactory.createSaab95();
+        saab.point.setLocation(0,100);
+        cc.cars.add(saab);
+
+        Car scania = CarFactory.createScania();
+        scania.point.setLocation(0,200);
+        cc.cars.add(scania);
+
+        // Start a new view and send a reference of self
+        this.frame = new CarView("CarSim 1.0", cc);
+
+        // Start the timer
+        cc.timer.start();
+    }
 
 
 
